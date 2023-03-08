52b91ada725de739c9b8c8780e498f95e3a7fafb
somu3341 <somashekarvsv1996@gmail.com>
2023-03-08 10:55:21 +0530

    [somu]Add:Printing the percentage of Head and Tail.

---
+++ FunctionalPrograms/FlipCoin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace FunctionalPrograms
+{
+    public class FlipCoin
+    {
+        const double VALIDATION_CHECK = 0.5;
+        public void Flip(int NumofTimes)
+        {
+            int head = 0, tail = 0;
+            Random random = new Random();
+            for (int i = 0; i < NumofTimes; i++)
+            {
+                double value = random.NextDouble();
+                if (value < VALIDATION_CHECK)
+                    tail++;
+                else
+                    head++;
+            }
+            double headPercentage = (head * 100 / NumofTimes);
+            double tailPercentage = (tail * 100 / NumofTimes);
+            Console.WriteLine("Head Percentage---->  " + headPercentage + "\n Tail Percentage---->  " + tailPercentage);
+        }
+    }
+}
+
---
+++ FunctionalPrograms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace FunctionalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional Programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose an option to execute\n1.Flip coin \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter num of flips");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        FlipCoin flip = new FlipCoin();
+                        flip.Flip(num);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+
+        }
+    }
+}
