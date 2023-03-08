edbe013517ca9df02105026d843b132a8ab6248e
somu3341 <somashekarvsv1996@gmail.com>
2023-03-08 16:31:02 +0530

    [somu]Refactor:Printing Harmonic value.

--- FunctionalPrograms/HarmonicNum.cs
+++ FunctionalPrograms/HarmonicNum.cs
@@ -3,17 +3,16 @@ namespace FunctionalPrograms
 {
     public class HarmonicNumber
     {
-        public void Harm(int num)
+        public void Harm(double num)
         {
-            int count;
+            double count;
             double result = 0;
             for (count = 1; count <= num; count++)
             {
-                Console.WriteLine("1/{0}+ ", count);
-                result += 1 / (float)count;
+                Console.WriteLine("1/"+count+"+",count);
+                result += 1 /count;
             }
             Console.WriteLine("Harmonic value of " + num + " is " + result);
-            Console.ReadKey();
         }
     }
 }
